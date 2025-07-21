4afd46abb1605d56ac743ca14eac3385769760b7
mazenElseify <58595140+mazenElseify@users.noreply.github.com>
2025-07-21 13:45:32 +0300

    Update MedicalRecordsDto and AppointmentDto files for extra efficient Mapping

--- DTOs/AppointmentDto.cs
+++ DTOs/AppointmentDto.cs
@@ -7,16 +7,28 @@ namespace ClinicSystem.API.DTOs
         public DateTime AppointmentDateTime { get; set; }
         public string Status { get; set; } = "Scheduled";
         public string? Reason { get; set; }
-        public int? CreatedBy { get; set; }
+        // public int? CreatedBy { get; set; }
     }
     public class UpdateAppointmentDto
+    {
+        // public int Id { get; set; }
+        // public int PatientId { get; set; }
+        // public int DoctorId { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string Status { get; set; } = "Scheduled";
+        public string? Reason { get; set; }
+        public string? Notes { get; set; }
+    }
+    public class AppointmentDetailsDto
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
         public int DoctorId { get; set; }
         public DateTime AppointmentDateTime { get; set; }
-        public string Status { get; set; } = "Scheduled";
+        public string Status { get; set; } = null!;
         public string? Reason { get; set; }
-        public int? CreatedBy { get; set; }
+        public string? Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
+
 }
--- DTOs/MedicalRecordDto.cs
+++ DTOs/MedicalRecordDto.cs
@@ -6,19 +6,41 @@ namespace ClinicSystem.API.DTOs
         public int DoctorId { get; set; }
         public DateTime VisitDate { get; set; }
         public string? Diagnosis { get; set; }
+        public string? Symptoms { get; set; }
         public string? Treatment { get; set; }
+        public string? Allergies { get; set; }
+        public string? Medications { get; set; }
         public int? CreatedBy { get; set; }
 
     }
     public class UpdateMedicalRecordDto
+    {
+        // public int Id { get; set; }
+        // public int PatientId { get; set; }
+        // public int DoctorId { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Symptoms { get; set; }
+        public string? Treatment { get; set; }
+        public string? Allergies { get; set; }
+        public string? Medications { get; set; }
+        public string? Notes { get; set; }
+
+    }
+    public class MedicalRecordDetailsDto
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
         public int DoctorId { get; set; }
         public DateTime VisitDate { get; set; }
         public string? Diagnosis { get; set; }
+        public string? Symptoms { get; set; }
         public string? Treatment { get; set; }
-        public int? CretedBy { get; set; }
-        
+        public string? Allergies { get; set; }
+        public string? Medications { get; set; }
+        public string? Notes { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
+
 }
