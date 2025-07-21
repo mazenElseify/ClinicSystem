a2a1960207a7a7b1d5cb9a58c47bdbb3b6c1f9ca
mazenElseify <58595140+mazenElseify@users.noreply.github.com>
2025-07-21 14:58:44 +0300

    InvoiceDto file created

---
+++ DTOs/InvoiceDto.cs
@@ -0,0 +1,37 @@
+namespace ClinicSystem.API.DTOs
+{
+    public class InvoceDto
+    {
+        public int PatientId { get; set; }
+        public int AppointmentId { get; set; }
+        public DateTime IssueDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; } = 0;
+        public string Status { get; set; } = "Unpaid";
+        public string? PaymentMethod { get; set; }
+        public string? Notes { get; set; }
+
+    }
+    public class UpdateInvoceDto
+    {
+        public decimal PaidAmount { get; set; }
+        public string? Status { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? Notes { get; set; }
+    }
+    public class InvoiceDetailsDto
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int AppointmentId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public string Status { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? Notes { get; set; }
+
+        public string? PatientName { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+    }
+}
