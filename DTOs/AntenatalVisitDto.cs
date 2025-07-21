ea2a9b5922c4bda2f9b5014de8064762b85d6402
mazenElseify <58595140+mazenElseify@users.noreply.github.com>
2025-07-21 14:11:18 +0300

    AntenatalVisitDto Created

---
+++ DTOs/AntenatalVisitDto.cs
@@ -0,0 +1,36 @@
+namespace ClinicSystem.API.DTOs
+{
+    public class AntenatalVissitDto
+    {
+        public int PregnancyId { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string? BloodPresure { get; set; }
+        public decimal? Weight { get; set; }
+        public int? FetalHeartRate { get; set; }
+        public string? UrineTestResult { get; set; }
+        public string? DoctorNotes { get; set; }
+
+    }
+    public class UpdateAntenatalVisitDto
+    {
+        public DateTime VisitDate { get; set; }
+        public string? BloodPresure { get; set; }
+        public decimal? Weight { get; set; }
+        public int? FetalHeartRate { get; set; }
+        public string? UrineTestResult { get; set; }
+        public string? DoctorNotes { get; set; }
+
+    }
+    public class UpdateAntenatalVisitDetailsDto
+    {
+        public int Id { get; set; }
+        public int PregnancyId { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string? BloodPresure { get; set; }
+        public decimal? Weight { get; set; }
+        public int? FetalHeartRate { get; set; }
+        public string? UrineTestResult { get; set; }
+        public string? DoctorNotes { get; set; }
+
+    }
+}
