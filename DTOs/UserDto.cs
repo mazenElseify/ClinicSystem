18c54ac959a187715115d7185049fa5da3f1ea10
mazenElseify <58595140+mazenElseify@users.noreply.github.com>
2025-07-21 11:44:58 +0300

    Update UserDto

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -11,6 +11,14 @@ namespace ClinicSystem.DTOs
         public bool IsActive { get; set; } = true;
 
     }
+    public class CreateUserDto
+{
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string Role { get; set; } = "Receptionist";
+}
     public class UpdateUserDto
     {
         public string? Email { get; set; }
